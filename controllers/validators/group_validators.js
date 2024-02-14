d8aea3f57b56f89e353770b76987d7697143c0d9
Anton Harbers <Anton.Harbers23@gmail.com>
2024-02-14 19:15:12 +0100

    Feat: Added updating, removing/adding members, replacing admin and deleting group controllers

--- controllers/validators/group_validators.js
+++ controllers/validators/group_validators.js
@@ -16,3 +16,28 @@ exports.post_group_validator = [
   body('message').optional().trim().escape(),
   body('profile_pic_url').optional().trim(),
 ];
+
+exports.update_group_validator = [
+  body('name').optional().trim().escape(),
+  body('message').optional().trim().escape(),
+  body('profile_pic_url').optional().trim(),
+];
+
+exports.replace_admin_validator = [
+  body('admin').trim().notEmpty().withMessage('admin should not be empty'),
+];
+
+exports.change_group_member_validator = [
+  body('member').trim().notEmpty().withMessage('member should not be empty'),
+];
+
+/*
+const GroupSchema = new Schema({
+  name: { type: String, default: 'group', require: true },
+  message: { type: String, default: 'new group' },
+  admin: { type: Schema.Types.ObjectId, ref: 'User', default: null },
+  members: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  profile_pic_url: { type: String, default: '' },
+  created_at: { type: Date, default: Date.now() },
+});
+*/
